084321ac98f5276e01828a834924850535b52d64
Vasilyev Maksim <maksim.vasilyev@pashabank.az>
2019-01-12 15:15:00 +0400

    Filtering by reviewer

--- src/components/ReviewScheduleTable.tsx
+++ src/components/ReviewScheduleTable.tsx
@@ -4,57 +4,96 @@ import * as React from 'react';
 import { Icon, Table } from 'semantic-ui-react';
 
 import { getReviewSchedule } from '../getReviewSchedule';
-import { ISquad } from '../models';
+import { IReviewer, ISquad } from '../models';
 import { Reviewer } from './Reviewer';
+import { ReviewScheduleTableFilter } from './ReviewScheduleTableFilter';
 
 interface IProps {
     squads: ISquad[];
 }
 
-export const ReviewScheduleTable: React.SFC<IProps> = ({ squads }) => {
-    const schedule = squads
-        ? getReviewSchedule(squads)
-        : [];
-    const _squads = schedule.length
-        ? schedule[0].reviewers.map((reviewer) => reviewer.squad)
-        : [];
+interface IState {
+    filteredBy: IReviewer;
+}
 
-    return (
-        <Table celled columns={(_squads.length + 1) as any}>
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell>
-                        <Icon name="calendar alternate outline" /> Date
+export class ReviewScheduleTable extends React.Component<IProps, IState> {
+    public state: IState = {
+        filteredBy: null,
+    };
+
+    private handleReviewerClick = (reviewer: IReviewer): void => {
+        this.setState({ filteredBy: reviewer });
+    }
+
+    private handleFilterClear = () => {
+        this.setState({ filteredBy: null });
+    }
+
+    public render (): JSX.Element {
+        const { squads } = this.props;
+
+        const schedule = squads
+            ? getReviewSchedule(squads).filter(
+                (day) => (
+                    !this.state.filteredBy ||
+                    day.reviewers.some(
+                        ({ reviewer }) => reviewer === this.state.filteredBy
+                    )
+                )
+            )
+            : [];
+        const _squads = schedule.length
+            ? schedule[0].reviewers.map((reviewer) => reviewer.squad)
+            : [];
+
+        return (
+            <>
+                <div style={{ textAlign: 'right' }}>
+                    <ReviewScheduleTableFilter
+                        reviewer={this.state.filteredBy}
+                        onClear={this.handleFilterClear}
+                    />
+                </div>
+
+                <Table celled columns={(_squads.length + 1) as any}>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>
+                                <Icon name="calendar alternate outline" /> Date
                     </Table.HeaderCell>
-                    {
-                        _squads.map((squad) => (
-                            <Table.HeaderCell key={squad.name}>
-                                <Icon name={squad.icon} /> {squad.name}
-                            </Table.HeaderCell>
-                        ))
-                    }
-                </Table.Row>
-            </Table.Header>
+                            {
+                                _squads.map((squad) => (
+                                    <Table.HeaderCell key={squad.name}>
+                                        <Icon name={squad.icon} /> {squad.name}
+                                    </Table.HeaderCell>
+                                ))
+                            }
+                        </Table.Row>
+                    </Table.Header>
 
-            <Table.Body>
-                {schedule.map(({ day, reviewers }) => (
-                    <Table.Row
-                        negative={day.isSame((moment()), 'day')}
-                        key={day.unix()}
-                    >
-                        <Table.Cell>
-                            {day.format('DD MMM YYYY')}
-                        </Table.Cell>
-                        {
-                            reviewers.map((squadReviewer) => (
-                                <Table.Cell key={squadReviewer.squad.name}>
-                                    <Reviewer reviewer={squadReviewer.reviewer} />
+                    <Table.Body>
+                        {schedule.map(({ day, reviewers }) => (
+                            <Table.Row
+                                negative={day.isSame((moment()), 'day')}
+                                key={day.unix()}
+                            >
+                                <Table.Cell>
+                                    {day.format('DD MMM YYYY')}
                                 </Table.Cell>
-                            ))
-                        }
-                    </Table.Row>
-                ))}
-            </Table.Body>
-        </Table>
-    );
-};
+                                {
+                                    reviewers.map(({ squad, reviewer }) => (
+                                        <Table.Cell key={squad.name} selectable>
+                                            <a href="#" onClick={() => this.handleReviewerClick(reviewer)}>
+                                                <Reviewer reviewer={reviewer} />
+                                            </a>
+                                        </Table.Cell>
+                                    ))
+                                }
+                            </Table.Row>
+                        ))}
+                    </Table.Body>
+                </Table>
+            </>
+        );
+    }
+}
---
+++ src/components/ReviewScheduleTableFilter.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { Icon, Label } from 'semantic-ui-react';
+
+import { IReviewer } from '../models';
+
+interface IProps {
+    reviewer: IReviewer;
+    onClear (): void;
+}
+
+export const ReviewScheduleTableFilter: React.SFC<IProps> = ({ reviewer, onClear }) => {
+    return (
+        <Label
+            size="medium"
+            image
+            onRemove={onClear}
+            removeIcon="delete"
+            style={{ visibility: reviewer ? 'visible' : 'hidden' }}
+            content={(
+                <>
+                    <img src={reviewer && reviewer.photo} />
+                    {reviewer && reviewer.name} {reviewer && reviewer.surname}
+                </>
+            )}
+        />
+    );
+};
