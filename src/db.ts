2231459be4aef3b3070bf087962f406bddd1703d
Vasilyev Maksim <maksim.vasilyev@pashabank.az>
2019-01-12 11:43:57 +0400

    Fixed firebase imports for production use

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 export const config = {
     apiKey: 'AIzaSyD7GbTdXaagmPBXX0tdX_O7U721Tb0q4pc',
