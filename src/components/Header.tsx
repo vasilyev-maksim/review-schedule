25e15c21746766977a77a566fb345f82776af514
Vasilyev Maksim <maksim.vasilyev@pashabank.az>
2019-01-13 00:02:33 +0400

    Typo fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Header as SemantiHeader, Icon } from 'semantic-ui-react';
+import { Header as SemanticHeader, Icon } from 'semantic-ui-react';
 
 export const Header: React.SFC = () => {
     return (
-        <SemantiHeader as="h2">
+        <SemanticHeader as="h2">
             <Icon name="list alternate outline" />
-            <SemantiHeader.Content>
+            <SemanticHeader.Content>
                 PR review schedule
-                <SemantiHeader.Subheader>
+                <SemanticHeader.Subheader>
                     for PASHA Bank Digital Lab developers
-                </SemantiHeader.Subheader>
-            </SemantiHeader.Content>
-        </SemantiHeader>
+                </SemanticHeader.Subheader>
+            </SemanticHeader.Content>
+        </SemanticHeader>
     );
 };
